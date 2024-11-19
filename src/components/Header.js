10c00a9cbd056528f5b178e7af84ef029f1febd9
Anita Wangui <anitawangui22@gmail.com>
2024-11-19 09:13:33 +0300

    add inverse flow using callbacks

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function Header({ isDarkMode, onDarkModeClick }) {
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </header>
+  );
+}
